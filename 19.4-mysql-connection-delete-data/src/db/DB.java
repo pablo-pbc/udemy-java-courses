9067e7347247d9085b8b5eafe3631b232337de30
Pablo de Brito Carvalho <pablo_pbc@outlook.com>
2025-04-19 14:01:09 -0300

    OrganizationalÃ: Updating the folders's names

---
+++ 19.4-mysql-connection-delete-data/src/db/DB.java
@@ -0,0 +1,68 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+
+    // Database connection variable
+    private static Connection conn = null;
+
+    // Function to connect the database
+    public static Connection getConnection() {
+        if (conn == null){
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    // Function to close the database's connection
+    public static void closeConnection(){
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    // Function to read the database's connection params
+    private static Properties loadProperties(){
+        try (FileInputStream fs = new FileInputStream("C:\\Users\\pablo\\Study\\udemy-java-courses\\19.0-mysql-connection-jdbc\\src\\db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+        if (st != null){
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+}
